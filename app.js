8ec3133d96f506f88d14d0a31ed0fd8249aeb4f8
Vivek Singh Mehta <viveksinghmehta.smr@gmail.com>
2019-03-12 23:06:41 +0530

    Body parser, stripe is added and the default index page

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 // this is my local server
+
+// Secret Key from stripe
+const secretKey = 'sk_test_Aw8X8i2E4jszw8KyM3Npyrcr';
+
+// Publishable Key from stripe
+const publishableKey = 'pk_test_A3PxLB8q0wcvXsHcDXOAumwU';
+
+
 var http = require('http');
 var express = require('express');
 var app = express();
+const stripe = require('stripe')(secretKey);
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(request, response) {
-    response.send('This server is made by vivek singh mehta');
+    response.sendfile('index.html');
 });
+
+app.post('/charge', function(request, response) {
+
+});
+
 var httpServer = http.createServer(app);
 
 httpServer.listen(process.env.PORT || 3000);
