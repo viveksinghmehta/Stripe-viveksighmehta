7180d4f23b308f6c14217bd50dca4d42723ba2e6
Vivek Singh Mehta <vivekmehta.frcoder@gmail.com>
2019-05-06 21:11:27 +0530

    New json in the response

--- app.js
+++ app.js
@@ -15,18 +15,44 @@ var publicDir = require('path').join(__dirname,'/public');
 const stripe = require('stripe')(secretKey);
 const bodyParser = require('body-parser');
 
-const morgan = require('morgan')
+const morgan = require('morgan');
 
 
-app.use(morgan('short'))
+app.use(morgan('short'));
 app.use(bodyParser.json());
 app.use(express.static(publicDir));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/user", function (request, response) {
-    let userJson = {firstName: "Vivek", lastName: "Mehta"}
-    response.json(userJson)
-})
+    let userJson = {
+        "id": "0001",
+        "type": "donut",
+        "name": "Cake",
+        "ppu": 0.55,
+        "batters":
+            {
+                "batter":
+                    [
+                        { "id": "1001", "type": "Regular" },
+                        { "id": "1002", "type": "Chocolate" },
+                        { "id": "1003", "type": "Blueberry" },
+                        { "id": "1004", "type": "Devil's Food" }
+                    ]
+            },
+        "topping":
+            [
+                { "id": "5001", "type": "None" },
+                { "id": "5002", "type": "Glazed" },
+                { "id": "5005", "type": "Sugar" },
+                { "id": "5007", "type": "Powdered Sugar" },
+                { "id": "5006", "type": "Chocolate with Sprinkles" },
+                { "id": "5003", "type": "Chocolate" },
+                { "id": "5004", "type": "Maple" }
+            ]
+    };
+    response.json(userJson);
+});
+
 
 const httpServer = http.createServer(app);
 
